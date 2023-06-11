ea975e3f55f961c9e1abacf551d3600bd5c52eb3
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-06-11 14:28:30 -0400

    Update SpecimenServiceStub

---
+++ src/main/java/com/plantplaces/service/SpecimenServiceStub.java
@@ -0,0 +1,32 @@
+package com.plantplaces.service;
+
+import org.springframework.stereotype.Component;
+
+import com.plantplaces.dto.SpecimenDTO;
+
+@Component
+public class SpecimenServiceStub implements ISpecimenService {
+	
+	/**(non-Javadoc)
+	 * @see com.plantplaces.service.ISpecimenService#fetchById(int)
+	 */
+	@Override
+	public SpecimenDTO fetchById(int id) {
+		 SpecimenDTO specimenDTO = new  SpecimenDTO();
+		 specimenDTO.setSpecimenId(43);
+		 specimenDTO.setLatitude("39.74");
+		 specimenDTO.setLongitude("-84.51");
+		 specimenDTO.setDescription("A beautiful Eastern Redbud");
+		 return specimenDTO;
+		
+	}
+
+	/**(non-Javadoc)
+	 * @see com.plantplaces.service.ISpecimenService#save(com.plantplaces.dto)
+	 */
+	@Override
+	public void save(SpecimenDTO specimenDTO) {
+		
+	}
+	
+}
